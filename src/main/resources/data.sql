fc0b63ef2ce1fd20f0a350a80957bec6b6300942
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-09-30 15:42:07 +0200

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- collegues
-insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('cql2405'     , 'Dupont', 'Jean' , 'dup@cqlTeam.go','1986-05-24','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
-insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('aze123456789', 'Humbut' , 'Marc'    , 'aze@cqlTeam.go'     ,'1992-08-12','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
-insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('qsd987654321', 'Gelaron', 'Delphine', 'qsd@cqlTeam.go'     ,'1990-04-26','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
-insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('wxc231564897', 'Dhalor' , 'Homer'   , 'wxc@cqlTeam.go'     ,'1953-07-17','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
-insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('ert879546213', 'kroche' , 'Sarah'   , 'ert@cqlTeam.go'     ,'1976-03-10','https://www.colocappart.ch/wp-content/uploads/2019/07/noimg1.jpg');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('cql2405'     , 'Dupont', 'Jean' , 'dup@cqlTeam.go','1986-05-24','https://risibank.fr/cache/stickers/d547/54725-full.png');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('aze123456789', 'Dupont' , 'Marc'    , 'aze@cqlTeam.go'     ,'1992-08-12','https://risibank.fr/cache/stickers/d547/54725-full.png');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('qsd987654321', 'Bean', 'Mr', 'qsd@cqlTeam.go'     ,'1990-04-26','https://miro.medium.com/proxy/1*VWK_hk_UuSsksK0ao_xMUQ.jpeg');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('wxc231564897', 'Simpson' , 'Homer'   , 'wxc@cqlTeam.go'     ,'1953-07-17','https://vignette.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png/revision/latest/top-crop/width/360/height/360?cb=20140126234206');
+insert into collegue(matricule, nom, prenom, email,date_naissance,photo_url) values ('ert879546213', 'kroche' , 'Sarah'   , 'ert@cqlTeam.go'     ,'1976-03-10','https://i.pinimg.com/originals/69/d7/2b/69d72bbf134fb93d29f64199d276ecfe.jpg');
 
